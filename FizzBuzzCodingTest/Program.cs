e56606c054846ee2c30a9f2066ddce2a4f141071
Connor Baker <connor.jacob.baker@live.com>
2023-11-17 14:17:55 +0000

    Add to the function to get another test working

--- FizzBuzzCodingTest/Program.cs
+++ FizzBuzzCodingTest/Program.cs
@@ -25,10 +25,14 @@ namespace FizzBuzzTest
         //Function to find if the value is divisible by 3, 5 or both
         public static string FindFizzBuzz(int number)
         {
+            //If the number is divisible by both 3 and 5 we need to return FizzBuzz
             if (number % 3 == 0 && number % 5 == 0)
             {
                 return "FizzBuzz";
             }
+            //If the number is divisble by only 3 return Fizz
+            else if (number % 3 == 0)
+                return "Fizz";
             else
             {
                 return "test";
