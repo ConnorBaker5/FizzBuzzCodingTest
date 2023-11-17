b846e8bdb356f8f5b9fb962981d760b4af7f5fce
Connor Baker <connor.jacob.baker@live.com>
2023-11-17 14:10:42 +0000

    Create new function and create tests for this to show them faling intitially

--- FizzBuzzCodingTest/Program.cs
+++ FizzBuzzCodingTest/Program.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FizzBuzzCodingTest
+namespace FizzBuzzTest
 {
-    internal class Program
+    public class FizzBuzz
     {
         static void Main(string[] args)
         {
+            int[] numbers = Enumerable.Range(1, 100).ToArray();
+
+            //lopp through the numbers calling the reusable function each time 
+            foreach (var number in numbers)
+            {
+                string result = FindFizzBuzz(number);
+
+                Console.WriteLine(result);
+            }
+        }
+
+        //Function to find if the value is divisible by 3, 5 or both
+        public static string FindFizzBuzz(int number)
+        {
+            return "test";
         }
     }
 }
--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -1,14 +1,62 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FizzBuzzTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
-namespace UnitTest
+namespace UnitTests
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ReturnNumber()
         {
+            //Arrange
+            string expectedResult = "1";
+
+            // Act 
+            string actualResult = FizzBuzz.FindFizzBuzz(1);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void ReturnFizz()
+        {
+            //Arrange
+            string expectedResult = "Fizz";
+
+            //Act
+            string actualResult = FizzBuzz.FindFizzBuzz(3);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void ReturnBuzz()
+        {
+            //Arrange
+            string expectedResult = "Buzz";
+
+            //Act
+            string actualResult = FizzBuzz.FindFizzBuzz(5);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void ReturnFizzBuzz()
+        {
+            //Arrange
+            string expectedResult = "FizzBuzz";
+
+            //Act
+            string actualResult = FizzBuzz.FindFizzBuzz(15);
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
         }
     }
 }
